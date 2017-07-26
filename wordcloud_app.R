7b6d83a25ea7397d2dbe545d3727178e2b310523
4bic <robyne.kiplangat@gmail.com>
2017-07-26 08:38:51 +0300

    getting started in R

---
+++ wordcloud_app.R
@@ -0,0 +1,11 @@
+#install
+install.packages("tm") #textmining
+install.packages("SnowballC") #text stemming
+install.packages("wordcloud") #word cloud generator
+install.packages("RColorBrewer") #color palettes
+
+#load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
